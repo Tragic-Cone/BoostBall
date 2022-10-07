c6ed1f35781704b0a275baf54167456ed75caad7
ReggieMlay <reggiereal7@gmail.com>
2022-10-07 14:16:10 -0500

    ball moves with the mouse
    The ball now moves with the mouse however couldn't figure out a way for the ball to ignore the y direction of the mouse.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -15,18 +15,22 @@ public class BallController : MonoBehaviour
         
         rigid = GetComponent<Rigidbody2D>();
     }
+    public float smoothTime = 0.3f;
+    Vector2 currentVelocity;
 
     private void Update()
     {
-        
-         if (Input.GetAxis("Horizontal") > 0)
-         {
-             rigid.AddForce(Vector3.right * speed);
-         }
-         else if (Input.GetAxis("Horizontal") < 0)
-         {
-             rigid.AddForce(-Vector3.right * speed);
-         }
+
+        /*if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigid.AddForce(Vector3.right * speed);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigid.AddForce(-Vector3.right * speed);
+        }*/
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, speed);
 
 
     }
