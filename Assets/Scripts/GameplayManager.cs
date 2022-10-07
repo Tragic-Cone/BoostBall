1792bee9128cb3449202ad31b16c3459ea374323
brewstercm <brewstercm@jacks.sfasu.edu>
2022-10-07 15:23:50 -0500

    Added UI overlay for gameplay

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+    private int health;
+    private bool hasShield;
+    private int numCoins;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 1;
+        hasShield = false;
+        numCoins = 0;
+        score = 0;
     }
 
     // Update is called once per frame
