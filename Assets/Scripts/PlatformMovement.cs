e460b0373d85460a291c2d2bb3ea2d6147ad2f29
ReggieMlay <reggiereal7@gmail.com>
2022-10-20 15:27:05 -0500

    Finished All the goals
    Sorry, it's a little late. Had to do a whole bunch of last minute changes

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -4,36 +4,12 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-
-    public Transform pos1, pos2;
-    public float speed;
-    public Transform startPos;
-
-    Vector3 nextPos;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        nextPos = startPos.position;    
-    }
-
-    // Update is called once per frame
+    public float movementSpeed = 5.0f;
+    int speed = 2;
+    //Your Update function
     void Update()
     {
-        if(transform.position == pos1.position)
-        {
-            nextPos = pos2.position;
-        }
-        if(transform.position == pos2.position)
-        {
-            nextPos = pos1.position;
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        transform.position = new Vector3(Mathf.PingPong(Time.time, 10), transform.position.y, transform.position.z);
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(pos1.position, pos2.position);
-    }
+    
 }
