aad4fa38f05b61fb99dca8c1e021ade21cf6e1ca
CadeBreedlove <97636382+CadeBreedlove@users.noreply.github.com>
2022-10-27 02:40:52 -0500

    finished enemy set up and started script for beam

---
+++ Assets/Scripts/LazerBeam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerBeam : MonoBehaviour
+{
+    public bool enabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.gameObject.name == "beam_12")
+        {
+            ToggleBeam();
+        }
+        else if (this.gameObject.name == "beam_0")
+        {
+            ToggleBeam();
+        }
+    }
+
+    void ToggleBeam()
+    {
+        enabled = !enabled;
+        foreach (Transform gameobject in gameObject.transform)
+        {
+            if (this.gameObject.name == "beam")
+            {
+                this.gameObject.SetActive(enabled);
+            }
+        }
+    }
+}
