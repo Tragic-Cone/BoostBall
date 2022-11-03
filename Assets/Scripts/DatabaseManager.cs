2e10e79470227679ba2e46fd5db55879844e1873
brewstercm <brewstercm@jacks.sfasu.edu>
2022-11-03 15:57:02 -0500

    removed nosql assets and made ui changes

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -1,14 +1,31 @@
-using Firebase;
-using Firebase.Database;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using TMPro;
 using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour
 {
+    public GameObject txtRegisterUsername;
+    public GameObject txtRegisterPassword;
+    public GameObject btnRegisterUser;
+
     // Start is called before the first frame update
     void Start()
     {
-            DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
+    }
+
+    public void RegisterUser(){
+        string txtUsername = txtRegisterUsername.GetComponent<TMP_InputField>().text;
+        string txtPassword = txtRegisterPassword.GetComponent<TMP_InputField>().text;
+        using(SqlConnection conn = DatabaseInstance.initializeDatabase()){
+            string query = $"INSERT INTO PLAYER VALUES('{txtUsername}', '{txtPassword}', {0})";
+            using(SqlCommand command = new SqlCommand(query, conn)){
+                conn.Open();
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
