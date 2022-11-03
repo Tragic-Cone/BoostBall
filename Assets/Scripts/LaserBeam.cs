c2b8a9ed3627c84d9854e0dfbfd1220e5071b2a6
CadeBreedlove <97636382+CadeBreedlove@users.noreply.github.com>
2022-11-03 16:11:52 -0500

    small updates

--- Assets/Scripts/LaserBeam.cs
+++ Assets/Scripts/LaserBeam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //CadeBreedlove
-public class LazerBeam : MonoBehaviour
+public class LaserBeam : MonoBehaviour
 {
     public bool enabled;
 
