67f896583d1f462f22bd7429a426e9ee962f24c9
CadeBreedlove <97636382+CadeBreedlove@users.noreply.github.com>
2022-11-26 20:44:23 -0600

    update on start game script and spawning object

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -8,11 +8,13 @@ public class StartGame : MonoBehaviour
     [SerializeField] GameObject Ball;
     [SerializeField] GameObject PlaceholderBall;
     [SerializeField] GameObject StartBoost;
+    [SerializeField] GameObject GenerationManager;
     public void clickedStart()
     {
         UIObject.SetActive(false);
         Ball.SetActive(true);
         PlaceholderBall.SetActive(false);
         StartBoost.SetActive(true);
+        GenerationManager.SetActive(true);
     }
 }
