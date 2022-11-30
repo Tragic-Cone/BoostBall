11514dbe7629e7db76146ea6942e79617420a42d
CadeBreedlove <97636382+CadeBreedlove@users.noreply.github.com>
2022-11-30 13:31:58 -0600

    small changes
    added on the platform tag to the holographic platform prefab. adjusted the platform spawning script to account for a time as well as if there is a certain number of platforms currently in the game instance.

--- Assets/Scripts/Breed's Scripts/PlatformSpawning.cs
+++ Assets/Scripts/Breed's Scripts/PlatformSpawning.cs
@@ -8,7 +8,7 @@ public class PlatformSpawning : MonoBehaviour
     public GameObject platformPrefab2;
     public GameObject platformPrefab3;
 
-    private float timeToSpawn = 3f;
+    private float timeToSpawn = 10f;
     public float currentTime = 0;
 
     void Start()
@@ -20,18 +20,23 @@ public class PlatformSpawning : MonoBehaviour
         spawnPosition.y += yPosition;
         spawnPosition.x = xPosition;
         Instantiate(platformPrefab1, spawnPosition, Quaternion.identity);
+       
     }
 
     void Update()
     {
+        GameObject[] GameobjectList = GameObject.FindGameObjectsWithTag("platform");
+
         Vector3 spawnPosition = new Vector3();
         float xPosition = Random.Range(-5f, 5f);
         float yPosition = 10f;
 
         currentTime += Time.deltaTime;
 
-        if (currentTime == timeToSpawn)
+        if (GameobjectList.Length < 3 && currentTime >= timeToSpawn)
         {
+            currentTime = 0;
+
             int pnum = Random.Range(1, 4);
             if (pnum == 1)
             {
@@ -51,7 +56,7 @@ public class PlatformSpawning : MonoBehaviour
                 spawnPosition.x = xPosition;
                 Instantiate(platformPrefab3, spawnPosition, Quaternion.identity);
             }
-            //platformCount++;
+            
         }
     }
 }
