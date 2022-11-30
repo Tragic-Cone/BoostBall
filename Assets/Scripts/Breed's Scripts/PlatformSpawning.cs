7b0601b9d959a25549eed18d9f2ceeeae02d3332
CadeBreedlove <97636382+CadeBreedlove@users.noreply.github.com>
2022-11-30 12:51:02 -0600

    changed the platform spawning script
    changed it to a timer based instead of a loop based spawning mechanic. Will be looking at changing it to when only so many platforms  are present spawn another to help with the fast scrolling mechanic of the game.

--- Assets/Scripts/Breed's Scripts/PlatformSpawning.cs
+++ Assets/Scripts/Breed's Scripts/PlatformSpawning.cs
@@ -8,14 +8,29 @@ public class PlatformSpawning : MonoBehaviour
     public GameObject platformPrefab2;
     public GameObject platformPrefab3;
 
-    public int platformCount = 3;
+    private float timeToSpawn = 3f;
+    public float currentTime = 0;
+
     void Start()
     {
         Vector3 spawnPosition = new Vector3();
         float xPosition = Random.Range(-5f, 5f);
         float yPosition = 10f;
 
-        for (int i = 0; i < platformCount; i++)
+        spawnPosition.y += yPosition;
+        spawnPosition.x = xPosition;
+        Instantiate(platformPrefab1, spawnPosition, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        Vector3 spawnPosition = new Vector3();
+        float xPosition = Random.Range(-5f, 5f);
+        float yPosition = 10f;
+
+        currentTime += Time.deltaTime;
+
+        if (currentTime == timeToSpawn)
         {
             int pnum = Random.Range(1, 4);
             if (pnum == 1)
@@ -38,8 +53,6 @@ public class PlatformSpawning : MonoBehaviour
             }
             //platformCount++;
         }
-
-
     }
 }
     
